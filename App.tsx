bfeee90ff0b9b30f723a95e4ea56e9b31ea26f4c
Usman Adepoju <adepojuusman465@gmail.com>
2024-08-22 17:38:31 +0100

    Add a shopping list item

--- App.tsx
+++ App.tsx
@@ -2,19 +2,31 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.itemContainer}>
+				<Text>Coffee</Text>
+				<StatusBar style="auto" />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		// alignItems: "center",
+		justifyContent: "center",
+	},
+	itemContainer: {
+		borderBottomWidth: 1,
+		borderBottomColor: "#1a759f",
+		paddingHorizontal: 8,
+		paddingVertical: 16,
+	},
+	itemText: {
+		fontSize: 18,
+		fontWeight: "200",
+	},
 });
