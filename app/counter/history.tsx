07f276c3b98af769ae6f47f78d262b22fdbaf7a6
Usman Adepoju <adepojuusman465@gmail.com>
2024-08-23 07:16:06 +0100

    Add a stack navigator for history

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,9 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function IdeaScreen() {
+export default function HistoryScreen() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Idea</Text>
+			<Text style={styles.text}>History</Text>
 		</View>
 	);
 }
